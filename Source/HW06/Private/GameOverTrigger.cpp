b45248386fdae220c3d990ad56f11a523698ddc4
oSiris1o7 <siuesen@gmail.com>
2025-07-08 23:32:31 +0900

    a13
    a13

---
+++ Source/HW06/Private/GameOverTrigger.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameOverTrigger.h"
+#include "Kismet/GameplayStatics.h"
+#include "MyCharacter.h"
+#include "GameFramework/SpringArmComponent.h" 
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AGameOverTrigger::AGameOverTrigger()
+{
+
+}
+
+// Called when the game starts or when spawned
+void AGameOverTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AGameOverTrigger::GameOverBegin()
+{
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	AMyCharacter* PlayerCharacter = Cast<AMyCharacter>(PlayerController ? PlayerController->GetPawn() : nullptr);
+
+	if (PlayerController && PlayerCharacter)
+	{
+		PlayerController->DisableInput(PlayerController);
+		PlayerCharacter->DisableInput(PlayerController);
+
+
+		PlayerCharacter->bUseControllerRotationPitch = false;
+		PlayerCharacter->bUseControllerRotationYaw = false;
+		PlayerCharacter->bUseControllerRotationRoll = false;
+
+
+		USpringArmComponent* SpringArm = PlayerCharacter->SpringArmComponent;
+		UCameraComponent* Camera = PlayerCharacter->CameraComponent;
+
+		if (SpringArm && Camera)
+		{
+			SpringArm->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
+			SpringArm->bUsePawnControlRotation = false;
+
+
+			//높이 조정? 현재 100으로 고정되잇으니까 변경바람..
+
+			FVector CurrentCameraLocation = Camera->GetComponentLocation();
+			Camera->SetWorldLocation(FVector(CurrentCameraLocation.X, CurrentCameraLocation.Y, 100.0f));
+
+		}
+
+
+
+
+
+
+	}
+
+}
---
+++ Source/HW06/Private/RotatingPlatformA.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RotatingPlatformA.h"
+
+// Sets default values
+ARotatingPlatformA::ARotatingPlatformA()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlatformMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlatformMesh"));
+	SetRootComponent(PlatformMesh);
+
+
+}
+
+// Called when the game starts or when spawned
+void ARotatingPlatformA::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	RandomRotation();
+
+	FVector Location = GetActorLocation();
+	Location.Z = ZPosition;
+	SetActorLocation(Location);
+}
+
+// Called every frame
+void ARotatingPlatformA::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FRotator CurrentRotation = PlatformMesh->GetRelativeRotation();
+
+	FRotator RotationDelta = FRotator(RotationSpeed.X * DeltaTime, RotationSpeed.X * DeltaTime, RotationSpeed.Z * DeltaTime);
+
+	CurrentRotation += RotationDelta;
+
+	PlatformMesh->SetRelativeRotation(CurrentRotation);
+}
+
+void ARotatingPlatformA::RandomRotation()
+{
+	RotationSpeed = FVector(0, 0, 0);
+	if (HoldRotationX == false)
+	{
+		RotationSpeed.X = FMath::RandRange(RotationTimeMin, RotationTimeMax);
+	}
+	if (HoldRotationY == false)
+	{
+		RotationSpeed.Y = FMath::RandRange(RotationTimeMin, RotationTimeMax);
+	}
+	if (HoldRotationZ == false)
+	{
+		RotationSpeed.Z = FMath::RandRange(RotationTimeMin, RotationTimeMax);
+	}
+
+}
